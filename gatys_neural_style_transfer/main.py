abddcf58eefba03a7b6f9d5e0ac3c3c5ade629a0
alpha-carinae29 <alpha.carinae29@gmail.com>
2020-05-30 03:00:30 +0430

    add structure of gatys style transfer function

---
+++ gatys_neural_style_transfer/main.py
@@ -0,0 +1,38 @@
+def load_and_preprocess_image(img):
+    pass
+
+
+def build_model(model_name):
+    pass
+
+
+def get_content_feature(model, content_image):
+    pass
+
+
+def get_style_features(model, style_image):
+    pass
+
+
+def compute_loss(content_feature, style_feature):
+    pass
+
+
+def apply_optimization_step(loss):
+    pass
+
+
+def initilize_image(method):
+    pass
+
+
+def log():
+    pass
+
+
+def stylize():
+    pass
+
+
+if __name__ == "__main__":
+    stylize()
